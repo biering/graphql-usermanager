0488d3ad71a2fbc42c8ac53ad45174f3f56b51cd
Chryb <chryb.q@gmail.com>
2017-09-24 10:49:18 +0200

    connect resolvers to mongodb

--- src/graphql/schema/resolvers/index.js
+++ src/graphql/schema/resolvers/index.js
@@ -1,17 +1,21 @@
+import mongo from 'mongodb'
+
+const toObjId = (id) =>
+  new mongo.ObjectID(id)
+
 // TMP
 const user0 = { id: 1, name: 'John Doe', email: 'john.doe@example.com' }
-const user1 = { id: 2, name: 'Max Mustermann', email: 'max.mustermann@example.com' }
-const user2 = { id: 3, name: 'Lahub Tatam', email: 'lahub.tatam@example.com' }
-
-const users = [user0, user1, user2]
 
 const Mutation = {
-  createUser (_, payload) {
-    // create user method
-    const user = Object.assign({ id: `${users.length + 1}` }, payload)
-    users.push(user)
-    return user
-  }
+  async createUser (root, payload, { mongo: { Users } }) {
+    const response = await Users.insert(payload)
+    return Object.assign({ _id: response.insertedIds[0] }, payload)
+  }/*,
+
+  async updateUser (root, payload, { mongo: { Users } }) {
+    const response = await Users.update({ _id: toObjId(payload.id) }, )
+    return Object.assign({ _id: response.insertedIds[0] }, payload)
+  }*/
 }
 
 const resolvers = {
@@ -20,12 +24,15 @@ const resolvers = {
       return user0
     },
 
-    user (_, payload) {
-      return users.find(u => u.id === payload.id)
+    async user (root, payload, { mongo: { Users } }) {
+      const user = await Users.findOne({ _id: toObjId(payload.id) })
+      if (user) { user.id = user._id }
+      return user
     },
 
-    allUsers (_, payload) {
-      return users
+    async allUsers (root, payload, { mongo: { Users } }) {
+      const users = await Users.find({}).toArray()
+      return users.map(user => Object.assign({ id: user._id }, user))
     }
   },
 
